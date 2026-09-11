bb57b3734dda96c961e94d15078b2403718d7aa2
Mark Shloss <MS17961@air-astana.net>
2026-09-11 10:11:22 +0500

    update activation script

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from pathlib import Path
 
 from scripts.init import initialize_project
+from scripts.activation_new_task import activate_new_task
 
+
+start_script: dict[str, bool] = {
+    'init': True,
+    'activate_proj': False,
+}
 
 BASE_DIR = Path('./')
 
@@ -11,11 +17,21 @@ LOG_DIR = BASE_DIR / 'logs'
 #  -----------------------------------------
 # | Проверка целостности структуры проекта |
 # -----------------------------------------
-list_dirs = [
-    LOG_DIR,
-]
+if start_script['init']:
+    list_dirs = [
+        LOG_DIR,
+    ]
 
-for dir in list_dirs:
-    if not dir.is_dir():
-        initialize_project(list_dirs=list_dirs)
-        
+    for dir in list_dirs:
+        if not dir.is_dir():
+            initialize_project(list_dirs=list_dirs)
+# ---------------------------------------------        
+
+#  -----------------------------------
+# | Система активации нового проекта |
+# -----------------------------------
+if start_script['activate_proj']:
+    PROJECTS_DIR = Path('../')
+
+    activate_new_task(base_dir=PROJECTS_DIR / 'active', name_of_task=str(), tmp_dir=PROJECTS_DIR / 'temp')
+# ---------------------------------------------
--- scripts/activation_new_task.py
+++ scripts/activation_new_task.py
@@ -1,24 +1,23 @@
 import os
 import shutil
-
-tmp_dir = "./temp"
-
-name_of_task = input('>>> ')
-
-base_dir = f'./active/{name_of_task}/'
-backup_dir = f'./active/{name_of_task}/backup/'
-
-if not os.path.isdir(base_dir):
-    os.mkdir(base_dir)
-    os.mkdir(backup_dir)
+from pathlib import Path
 
 
-src_files = os.listdir(tmp_dir)
-for file_name in src_files:
-    full_file_name = os.path.join(tmp_dir, file_name)
-    # print(repr(full_file_name))
-    if os.path.isfile(full_file_name):
-        shutil.copy(full_file_name, base_dir)
-        if full_file_name != './temp\\template.ipynb':
-            shutil.move(full_file_name, backup_dir)
-              
+def activate_new_task(base_dir: Path, name_of_task: str, tmp_dir: Path) -> None:
+
+    base_dir = base_dir / {name_of_task} 
+    backup_dir = base_dir / {name_of_task} / 'backup'
+
+    if not os.path.isdir(base_dir):
+        os.mkdir(base_dir)
+        os.mkdir(backup_dir)
+
+
+    src_files = os.listdir(tmp_dir)
+    for file_name in src_files:
+        full_file_name = os.path.join(tmp_dir, file_name)
+        # print(repr(full_file_name))
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, base_dir)
+            if full_file_name != './temp\\template.ipynb':
+                shutil.move(full_file_name, backup_dir)
